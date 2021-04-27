b0524ba9753be92e1969ecd31a5e1cf97c77ab79
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-27 08:20:51 -0300

    Interrupção

---
+++ 16F628A/C - Interrupção/C_Interrupt.c
@@ -0,0 +1,49 @@
+#include <xc.h>
+//__CONFIG(BOREN_OFF & MCLRE_OFF & CP_OFF & WDTE_OFF & FOSC_INTOSCIO);
+#define _XTAL_FREQ  4000000
+
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF     // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF          // Flash Program Memory Code Protection bit (0000h to 07FFh code-protected)
+
+__interrupt(high_priority) void getData(void){
+            GIE = 0;
+            if (INTE && INTF){
+                RA0 = !RA0;
+                INTF = 0;
+            }else if (T0IE && T0IF){
+                RB1 = !RB1;
+                TMR0 = 5;
+                T0IF = 0;
+            }
+            GIE = 1;    
+        }
+
+void main(){
+
+    PORTB = 0;
+    PORTA = 0;
+    TRISA = 0b11111110;
+    TRISB = 0b10000001;
+    OPTION_REG = 0b01000111;
+    INTCON = 0b10110000;
+    CMCON = 7;
+
+    int i;
+    int cont = 0;
+  
+    TMR0 = 6;
+    
+    while(1){ 
+        
+        RB3 = !RB3;
+        __delay_ms(500);
+   
+    }
+}
